efcaf9c9585bef96a63d33336c5623411f5a23d8
Lee Fogg <leefogg@users.noreply.github.com>
2020-07-11 22:04:22 +0100

    Polished Demotronic effect

--- src/effects/Demotronic.asm
+++ src/effects/Demotronic.asm
@@ -9,13 +9,6 @@ HandleVBlank:
     POP HL ; 3
     JP WaitToStartEffect ; 4
 
-SECTION "STAT Interrupt", ROM0[$48]
-HandleSTAT:
-    EI
-
-    POP HL
-    JP WaitToStartEffect
-
 SECTION "Boot Vector", ROM0[$100]
     JR Main
 
@@ -32,10 +25,6 @@ Main:
     CALL PrepareVariables
 
     CALL EnableVBlank
-
-    LD HL, rIF
-    XOR A
-    LD [HL], A
 
     ; Turn on LCD
     LD HL, rLCDC
@@ -157,19 +146,20 @@ StartEffect:
     NOP
     ; Mode 0 (HBlank) starts here
     INC A ; This is pointless at runtime. Its just here as a marker for easier debugging.
-    ; This takes up the remaing time between HBlank and Mode3 - 70 cycles
-    REPT 64
-        NOP 
-    ENDR
 
     ; Copy next scanline's data
     LD HL, CurrentScanlineData
     ; Demotronic's table has a stride of 11 but it only uses 10, no idea why yet
-    REPT 10         ; 70
+    REPT 10         ; 7 * 10
         LD A, [DE]  ; 2
         ADD A, B    ; 1
         INC DE      ; 2
         LD [HL+], A ; 2
+    ENDR
+
+    ; This takes up the remaing time between HBlank and Mode3
+    REPT 64
+        NOP 
     ENDR
 
     ; Change SCY each tile according to LUT
@@ -177,7 +167,7 @@ StartEffect:
     ; Mode 3 starts here
     REPT 9
         LD A, [HL+]     ; 2
-        LD [$FF00+C], A ; 2. 
+        LD [$FF00+C], A ; 2
     ENDR
     LD   A,[HL]         ; 2
     LD   [$FF00+C],A    ; 2
@@ -188,12 +178,11 @@ StartEffect:
     JP .Loop ; 4
 
 SECTION "Scanline Data", ROM0, ALIGN[8]
-; 1440 bytes. Each byte is a SCY value to set each tile each scanline
+; 144 * 10 bytes. Each byte is a SCY value to set each tile each scanline
 AllScanlineData:
 DB $24, $1E, $18, $15, $0F, $0B, $07, $06, $04, $02, $23, $1E, $18, $14, $0F, $0B, $07, $06, $04, $02, $23, $1D, $18, $14, $0F, $0B, $07, $05, $04, $02, $22, $1D, $17, $14, $0E, $0B, $07, $05, $04, $02, $22, $1C, $17, $13, $0E, $0B, $07, $05, $04, $02, $21, $1C, $17, $13, $0E, $0A, $07, $05, $03, $02, $21, $1B, $16, $13, $0E, $0A, $07, $05, $03, $02, $20, $1B, $16, $13, $0E, $0A, $07, $05, $03, $02, $20, $1B, $16, $12, $0D, $0A, $07, $05, $03, $02, $1F, $1A, $15, $12, $0D, $0A, $07, $05, $03, $02, $1F, $1A, $15, $12, $0D, $0A, $06, $05, $03, $02, $1E, $19, $15, $11, $0D, $0A, $06, $05, $03, $02, $1E, $19, $14, $11, $0C, $09, $06, $05, $03, $02, $1D, $19, $14, $11, $0C, $09, $06, $05, $03, $02, $1D, $18, $14, $11, $0C, $09, $06, $05, $03, $02, $1C, $18, $13, $10, $0C, $09, $06, $04, $03, $01, $1C, $17, $13, $10, $0C, $09, $06, $04, $03, $01, $1B, $17, $13, $10, $0B, $09, $06, $04, $03, $01, $1B, $16, $12, $0F, $0B, $08, $06, $04, $03, $01, $1A, $16, $12, $0F, $0B, $08, $06, $04, $03, $01, $1A, $16, $12, $0F, $0B, $08, $05, $04, $03, $01, $19, $15, $11, $0F, $0B, $08, $05, $04, $03, $01, $19, $15, $11, $0E, $0A, $08, $05, $04, $03, $01, $18, $14, $11, $0E, $0A, $08, $05, $04, $03, $01, $18, $14, $10, $0E, $0A, $07, $05, $04, $02, $01, $17, $14, $10, $0D, $0A, $07, $05, $04, $02, $01, $17, $13, $10, $0D, $0A, $07, $05, $04, $02, $01, $16, $13, $0F, $0D, $09, $07, $05, $04, $02, $01, $16, $12, $0F, $0D, $09, $07, $05, $03, $02, $01, $15, $12, $0F, $0C, $09, $07, $04, $03, $02, $01, $15, $11, $0E, $0C, $09, $07, $04, $03, $02, $01, $14, $11, $0E, $0C, $09, $06, $04, $03, $02, $01, $14, $11, $0E, $0B, $08, $06, $04, $03, $02, $01, $13, $10, $0D, $0B, $08, $06, $04, $03, $02, $01, $13, $10, $0D, $0B, $08, $06, $04, $03, $02, $01, $12, $0F, $0D, $0B, $08, $06, $04, $03, $02, $01, $12, $0F, $0C, $0A, $07, $06, $04, $03, $02, $01, $11, $0F, $0C, $0A, $07, $05, $04, $03, $02, $01, $11, $0E, $0B, $0A, $07, $05, $04, $03, $02, $01, $10, $0E, $0B, $09, $07, $05, $03, $03, $02, $01, $10, $0D, $0B, $09, $07, $05, $03, $02, $02, $01, $0F, $0D, $0A, $09, $06, $05, $03, $02, $02, $01, $0F, $0C, $0A, $09, $06, $05, $03, $02, $02, $01, $0E, $0C, $0A, $08, $06, $05, $03, $02, $02, $01, $0E, $0C, $09, $08, $06, $04, $03, $02, $01, $01, $0D, $0B, $09, $08, $06, $04, $03, $02, $01, $01, $0D, $0B, $09, $07, $05, $04, $03, $02, $01, $01, $0C, $0A, $08, $07, $05, $04, $03, $02, $01, $01, $0C, $0A, $08, $07, $05, $04, $02, $02, $01, $01, $0B, $0A, $08, $07, $05, $04, $02, $02, $01, $01, $0B, $09, $07, $06, $05, $03, $02, $02, $01, $01, $0A, $09, $07, $06, $04, $03, $02, $02, $01, $01, $0A, $08, $07, $06, $04, $03, $02, $02, $01, $01, $09, $08, $06, $05, $04, $03, $02, $01, $01, $00, $09, $07, $06, $05, $04, $03, $02, $01, $01, $00, $08, $07, $06, $05, $04, $03, $02, $01, $01, $00, $08, $07, $05, $05, $03, $02, $02, $01, $01, $00, $07, $06, $05, $04, $03, $02, $02, $01, $01, $00, $07, $06, $05, $04, $03, $02, $01, $01, $01, $00, $06, $05, $04, $04, $03, $02, $01, $01, $01, $00, $06, $05, $04, $03, $02, $02, $01, $01, $01, $00, $05, $05, $04, $03, $02, $02, $01, $01, $01, $00, $05, $04, $03, $03, $02, $02, $01, $01, $01, $00, $04, $04, $03, $03, $02, $01, $01, $01, $00, $00, $04, $03, $03, $02, $02, $01, $01, $01, $00, $00, $03, $03, $02, $02, $01, $01, $01, $01, $00, $00, $03, $02, $02, $02, $01, $01, $01, $00, $00, $00, $02, $02, $02, $01, $01, $01, $01, $00, $00, $00, $02, $02, $01, $01, $01, $01, $00, $00, $00, $00, $01, $01, $01, $01, $01, $00, $00, $00, $00, $00, $01, $01, $01, $01, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $FF, $FF, $FF, $FF, $00, $00, $00, $00, $00, $00, $FF, $FF, $FF, $FF, $FF, $00, $00, $00, $00, $00, $FE, $FE, $FF, $FF, $FF, $FF, $00, $00, $00, $00, $FE, $FE, $FE, $FF, $FF, $FF, $FF, $00, $00, $00, $FD, $FE, $FE, $FE, $FF, $FF, $FF, $00, $00, $00, $FD, $FD, $FE, $FE, $FF, $FF, $FF, $FF, $00, $00, $FC, $FD, $FD, $FE, $FE, $FF, $FF, $FF, $00, $00, $FC, $FC, $FD, $FD, $FE, $FF, $FF, $FF, $00, $00, $FB, $FC, $FD, $FD, $FE, $FE, $FF, $FF, $FF, $00, $FB, $FB, $FC, $FD, $FE, $FE, $FF, $FF, $FF, $00, $FA, $FB, $FC, $FD, $FE, $FE, $FF, $FF, $FF, $00, $FA, $FB, $FC, $FC, $FD, $FE, $FF, $FF, $FF, $00, $F9, $FA, $FB, $FC, $FD, $FE, $FF, $FF, $FF, $00, $F9, $FA, $FB, $FC, $FD, $FE, $FE, $FF, $FF, $00, $F8, $F9, $FB, $FB, $FD, $FE, $FE, $FF, $FF, $00, $F8, $F9, $FA, $FB, $FC, $FD, $FE, $FF, $FF, $00, $F7, $F9, $FA, $FB, $FC, $FD, $FE, $FF, $FF, $00, $F7, $F8, $FA, $FB, $FC, $FD, $FE, $FF, $FF, $00, $F6, $F8, $F9, $FA, $FC, $FD, $FE, $FE, $FF, $FF, $F6, $F7, $F9, $FA, $FC, $FD, $FE, $FE, $FF, $FF, $F5, $F7, $F9, $FA, $FB, $FD, $FE, $FE, $FF, $FF, $F5, $F6, $F8, $F9, $FB, $FC, $FE, $FE, $FF, $FF, $F4, $F6, $F8, $F9, $FB, $FC, $FE, $FE, $FF, $FF, $F4, $F6, $F8, $F9, $FB, $FC, $FD, $FE, $FF, $FF, $F3, $F5, $F7, $F9, $FB, $FC, $FD, $FE, $FF, $FF, $F3, $F5, $F7, $F8, $FA, $FC, $FD, $FE, $FF, $FF, $F2, $F4, $F7, $F8, $FA, $FC, $FD, $FE, $FF, $FF, $F2, $F4, $F6, $F8, $FA, $FB, $FD, $FE, $FE, $FF, $F1, $F4, $F6, $F7, $FA, $FB, $FD, $FE, $FE, $FF, $F1, $F3, $F6, $F7, $FA, $FB, $FD, $FE, $FE, $FF, $F0, $F3, $F5, $F7, $F9, $FB, $FD, $FE, $FE, $FF, $F0, $F2, $F5, $F7, $F9, $FB, $FD, $FD, $FE, $FF, $EF, $F2, $F5, $F6, $F9, $FB, $FC, $FD, $FE, $FF, $EF, $F1, $F4, $F6, $F9, $FB, $FC, $FD, $FE, $FF, $EE, $F1, $F4, $F6, $F9, $FA, $FC, $FD, $FE, $FF, $EE, $F1, $F3, $F5, $F8, $FA, $FC, $FD, $FE, $FF, $ED, $F0, $F3, $F5, $F8, $FA, $FC, $FD, $FE, $FF, $ED, $F0, $F3, $F5, $F8, $FA, $FC, $FD, $FE, $FF, $EC, $EF, $F2, $F5, $F8, $FA, $FC, $FD, $FE, $FF, $EC, $EF, $F2, $F4, $F7, $FA, $FC, $FD, $FE, $FF, $EB, $EF, $F2, $F4, $F7, $F9, $FC, $FD, $FE, $FF, $EB, $EE, $F1, $F4, $F7, $F9, $FC, $FD, $FE, $FF, $EA, $EE, $F1, $F3, $F7, $F9, $FB, $FD, $FE, $FF, $EA, $ED, $F1, $F3, $F7, $F9, $FB, $FC, $FE, $FF, $E9, $ED, $F0, $F3, $F6, $F9, $FB, $FC, $FE, $FF, $E9, $EC, $F0, $F3, $F6, $F9, $FB, $FC, $FE, $FF, $E8, $EC, $F0, $F2, $F6, $F9, $FB, $FC, $FE, $FF, $E8, $EC, $EF, $F2, $F6, $F8, $FB, $FC, $FD, $FF, $E7, $EB, $EF, $F2, $F6, $F8, $FB, $FC, $FD, $FF, $E7, $EB, $EF, $F1, $F5, $F8, $FB, $FC, $FD, $FF, $E6, $EA, $EE, $F1, $F5, $F8, $FB, $FC, $FD, $FF, $E6, $EA, $EE, $F1, $F5, $F8, $FA, $FC, $FD, $FF, $E5, $EA, $EE, $F1, $F5, $F8, $FA, $FC, $FD, $FF, $E5, $E9, $ED, $F0, $F5, $F7, $FA, $FC, $FD, $FF, $E4, $E9, $ED, $F0, $F4, $F7, $FA, $FC, $FD, $FF, $E4, $E8, $ED, $F0, $F4, $F7, $FA, $FC, $FD, $FF, $E3, $E8, $EC, $EF, $F4, $F7, $FA, $FB, $FD, $FE, $E3, $E7, $EC, $EF, $F4, $F7, $FA, $FB, $FD, $FE, $E2, $E7, $EC, $EF, $F4, $F7, $FA, $FB, $FD, $FE, $E2, $E7, $EB, $EF, $F3, $F6, $FA, $FB, $FD, $FE, $E1, $E6, $EB, $EE, $F3, $F6, $FA, $FB, $FD, $FE, $E1, $E6, $EB, $EE, $F3, $F6, $F9, $FB, $FD, $FE, $E0, $E5, $EA, $EE, $F3, $F6, $F9, $FB, $FD, $FE, $E0, $E5, $EA, $ED, $F2, $F6, $F9, $FB, $FD, $FE, $DF, $E5, $EA, $ED, $F2, $F6, $F9, $FB, $FD, $FE, $DF, $E4, $E9, $ED, $F2, $F6, $F9, $FB, $FD, $FE, $DE, $E4, $E9, $ED, $F2, $F5, $F9, $FB, $FC, $FE, $DE, $E3, $E9, $EC, $F2, $F5, $F9, $FB, $FC, $FE, $DD, $E3, $E8, $EC, $F1, $F5, $F9, $FB, $FC, $FE, $DD, $E2, $E8, $EC, $F1, $F5, $F9, $FA, $FC, $FE, $DC
 
 SECTION "Variables", WRAM0
-; The 10 byte SCY array to use for this scanline
 CurrentScanlineData: ds 1
 SECTION "HRAM Variables", HRAM
 ; Current frame counter, offsets CurrentScanlineData to make the screen go down
